77dd44f20c9f0acaa741180254972cf86e95e993
yangyile <gogit@github.com/go-xlan/gogit>
2025-03-09 21:50:22 +0700

    让主要逻辑复用英文版项目

--- get获得日志.go
+++ get获得日志.go
@@ -1,13 +1,16 @@
 package zapzhkratos
 
-import "github.com/go-kratos/kratos/v2/log"
+import (
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/orzkratos/zapkratos"
+)
 
 func (A *T匝普日志) Get奎沱日志(msg模块 string) log.Logger {
-	return New奎沱日志(A.Get基本匝普().LOG, msg模块)
+	return zapkratos.NewLogImp(A.Get基本匝普().LOG, msg模块)
 }
 
 func (A *T匝普日志) New奎沱日志(msg模块 string) log.Logger {
-	return New奎沱日志(A.Get基本匝普().LOG, msg模块)
+	return zapkratos.NewLogImp(A.Get基本匝普().LOG, msg模块)
 }
 
 func (A *T匝普日志) Get奎沱主簿(msg模块 string) *log.Helper {
--- new匝普封装.go
+++ new匝普封装.go
@@ -1,44 +1,37 @@
 package zapzhkratos
 
 import (
-	"path/filepath"
-
-	"github.com/yyle88/runpath"
+	"github.com/orzkratos/zapkratos"
 	"github.com/yyle88/zaplog"
 )
 
-type T日志配置 struct {
-	V模块位置键名 string
-}
+type T日志配置 zapkratos.Options
 
 func New日志配置() *T日志配置 {
-	return &T日志配置{
-		V模块位置键名: "模块位置",
-	}
+	p := zapkratos.NewOptions()
+	p.WithModuleKeyName("模块位置")
+	return (*T日志配置)(p)
 }
 
 func (T *T日志配置) With模块位置键名(v模块位置键名 string) *T日志配置 {
-	T.V模块位置键名 = v模块位置键名
-	return T
+	p := (*zapkratos.Options)(T)
+	p.WithModuleKeyName(v模块位置键名)
+	return (*T日志配置)(p)
 }
 
 // T匝普日志 这里主要做的就是把英文的类型名改为中文类型名，再增加几个简单的逻辑
-type T匝普日志 struct {
-	zap *zaplog.Zap
-	opt *T日志配置
-}
+type T匝普日志 zapkratos.ZapKratos
 
-func New匝普日志(zap *zaplog.Zap, opt *T日志配置) *T匝普日志 {
-	return &T匝普日志{
-		zap: zap,
-		opt: opt,
-	}
+func New匝普日志(zap *zaplog.Zap, options *T日志配置) *T匝普日志 {
+	return (*T匝普日志)(zapkratos.NewZapKratos(zap, (*zapkratos.Options)(options)))
 }
 
 func (A *T匝普日志) Get基本匝普() *zaplog.Zap {
-	return A.zap
+	p := (*zapkratos.ZapKratos)(A)
+	return p.GetZap()
 }
 
 func (A *T匝普日志) Sub模块匝普() *zaplog.Zap {
-	return A.Get基本匝普().SubZap2(A.opt.V模块位置键名, filepath.Base(runpath.Skip(1)))
+	p := (*zapkratos.ZapKratos)(A)
+	return p.SubZap()
 }
